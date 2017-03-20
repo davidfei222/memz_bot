f98876e3fb0f4ad3d54459008ced18a6318dfd8d
davidfei222 <dfei@wisc.edu>
2017-03-20 18:31:46 -0500

    bug fixes

--- memz_bot.js
+++ memz_bot.js
@@ -7,32 +7,44 @@ var fs = require("fs");
 var triggerPhrases;
 var jalen;
 var louis;
-fs.readFile('triggers.json',function(err, content){
-  if(err) throw err;
-  triggerPhrases = JSON.parse(content);
-  jalen = triggerPhrases.jalenPhrases;
-  louis = triggerPhrases.louisPhrases;
-  //console.log(triggerPhrases);
-});
+
 
 //Let's change some settings!
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    colorize : true
+  colorize : true
 });
 logger.level = 'debug';
 
 //Here we create our bot variable, this is what we're going to use to communicate to discord.
 var bot = new Discord.Client({
-    token: "MjkyNTMxOTA3MjEwNTEwMzM4.C688OA.rfzVVF7krlO7kOntKuX3udT49lg",
-    autorun: true,
-    messageCacheLimit: 100
+  token: "MjkyNTMxOTA3MjEwNTEwMzM4.C688OA.rfzVVF7krlO7kOntKuX3udT49lg",
+  autorun: true,
+  messageCacheLimit: 100
 });
 
 bot.on("ready", function (rawEvent) {
-    logger.info("Connected!");
-    logger.info("Logged in as: ");
-    logger.info(bot.username + " - (" + bot.id + ")");
+  logger.info("Connected!");
+  logger.info("Logged in as: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
+});
+
+//Check if Jalen or Louis is online and reload the triggers JSON file accordingly
+bot.on("presence", function (user, userID, status, game, event) {
+  console.log("Someone has logged in.");
+  if((userID == "279845556166197251" || userID == "285178566751158273") && status == "online") {
+    console.log("Checking if Jalen or Louis is online...");
+    fs.readFile('triggers.json',function(err, content){
+      if(err) throw err;
+      triggerPhrases = JSON.parse(content);
+      jalen = triggerPhrases.jalenPhrases;
+      louis = triggerPhrases.louisPhrases;
+      //console.log(triggerPhrases);
+    });
+  }
+  if(userID == "285182845519921152" && status == "online"){
+    console.log("Master is online.");
+  }
 });
 
 //In this function we're going to add our commands. This set of commands is triggered whenever a new message is sent to a channel.
@@ -50,10 +62,13 @@ bot.on("message", function (user, userID, channelID, message, rawEvent) {
   //console.log(ownerID);
 
   if (userID == "279845556166197251") {
-    triggerPhrases.jalenPhrases.push(message);
-    fs.writeFile('triggers.json', JSON.stringify(triggerPhrases), function(err){
-      if(err) throw err;
-    });
+    if(jalen.indexOf(message) < 0){
+      triggerPhrases.jalenPhrases.push(message);
+      jalen.push(message);
+      fs.writeFile('triggers.json', JSON.stringify(triggerPhrases), function(err){
+        if(err) throw err;
+      });
+    }
     for(i = 0; i < jalen.length; i++) {
       if (message == jalen[i] || message.includes(jalen[i])) { //If Jalen says something stupid, we'll do something!
         bot.sendMessage({ //We're going to send him a message!
@@ -73,13 +88,9 @@ bot.on("message", function (user, userID, channelID, message, rawEvent) {
     }
   }
 
-  if (userID == "285178566751158273" ) {
-    /*triggerPhrases.louisPhrases.push(message);
-    fs.writeFile('triggers.json', JSON.stringify(triggerPhrases), function(err){
-      if(err) throw err;
-    });*/
+  if (userID == "285178566751158273") {
     for(i = 0; i < louis.length; i++) {
-      if (message == louis[i] || message.includes(louis[i])) { //If Louis says something stupid, we'll do something!
+      if (message == louis[i] || message.includes(louis[i])) { //If Louis starts projecting, we'll do something!
         bot.sendMessage({ //We're going to send him a message!
           to : channelID,
           message : "Shut the fuck up Louis"
@@ -109,15 +120,11 @@ bot.on("message", function (user, userID, channelID, message, rawEvent) {
       bot.editUserInfo({
         username: arguments[1]
       });
+      bot.sendMessage({
+        to : channelID,
+        message : "A username change has been attempted for the bot. If the name change was unsuccessful, it is because Discord's name change cooldown is active."
+      });
     }
   }
-
-  /*if (message != "Any viewpoint expressed by Travis Vetter is automatically negated by the fact that he gets off to digital cartoon girls."){
-    bot.sendMessage({
-      to : channelID,
-      message : "Any viewpoint expressed by Travis Vetter is automatically negated by the fact that he gets off to digital cartoon girls."
-    });
-    logger.info("Bot triggered at " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-  }*/
 
 });
