bc0f07d7d8e15f6c6bb61510d097b9af73ae3ecf
davidfei222 <dfei@wisc.edu>
2017-03-20 20:10:23 -0500

    cleaned up the code

--- memz_bot.js
+++ memz_bot.js
@@ -3,11 +3,10 @@ var logger = require("winston");
 var Discord = require("discord.io");
 var fs = require("fs");
 
-//Load the trigger phrases from the local JSON file
+//Variables for loading the trigger phrases from the local JSON file
 var triggerPhrases;
 var jalen;
 var louis;
-
 
 //Let's change some settings!
 logger.remove(logger.transports.Console);
@@ -30,7 +29,7 @@ bot.on("ready", function (rawEvent) {
 });
 
 //Check if Jalen or Louis is online and reload the triggers JSON file accordingly
-bot.on("presence", function (user, userID, status, game, event) {
+/*bot.on("presence", function (user, userID, status, game, event) {
   console.log("Someone has logged in.");
   if((userID == "279845556166197251" || userID == "285178566751158273") && status == "online") {
     console.log("Checking if Jalen or Louis is online...");
@@ -45,7 +44,7 @@ bot.on("presence", function (user, userID, status, game, event) {
   if(userID == "285182845519921152" && status == "online"){
     console.log("Master is online.");
   }
-});
+});*/
 
 //In this function we're going to add our commands. This set of commands is triggered whenever a new message is sent to a channel.
 bot.on("message", function (user, userID, channelID, message, rawEvent) {
@@ -61,14 +60,13 @@ bot.on("message", function (user, userID, channelID, message, rawEvent) {
   //console.log(serverID);
   //console.log(ownerID);
 
-  if (userID == "279845556166197251") {
-    if(jalen.indexOf(message) < 0){
-      triggerPhrases.jalenPhrases.push(message);
-      jalen.push(message);
-      fs.writeFile('triggers.json', JSON.stringify(triggerPhrases), function(err){
-        if(err) throw err;
-      });
-    }
+  if (userID == "279845556166197251") { //Entered when Jalen messages the channel
+    fs.readFile('triggers.json',function(err, content){ //Read the most up to date list of trigger phrases
+      if(err) throw err;
+      triggerPhrases = JSON.parse(content);
+      jalen = triggerPhrases.jalenPhrases;
+      //console.log(triggerPhrases);
+    });
     for(i = 0; i < jalen.length; i++) {
       if (message == jalen[i] || message.includes(jalen[i])) { //If Jalen says something stupid, we'll do something!
         bot.sendMessage({ //We're going to send him a message!
@@ -86,9 +84,21 @@ bot.on("message", function (user, userID, channelID, message, rawEvent) {
         logger.info("Kicked Jalen out of the server for being autistic at " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
       }
     }
+    if(jalen.indexOf(message) < 0){ //Add the phrase he just said into the list for future reference
+      triggerPhrases.jalenPhrases.push(message);
+      fs.writeFile('triggers.json', JSON.stringify(triggerPhrases), function(err){
+        if(err) throw err;
+      });
+    }
   }
 
-  if (userID == "285178566751158273") {
+  if (userID == "285178566751158273") { //Entered when louis messages the channel
+    fs.readFile('triggers.json',function(err, content){ //Read the most up to date list of trigger phrases
+      if(err) throw err;
+      triggerPhrases = JSON.parse(content);
+      louis = triggerPhrases.louisPhrases;
+      //console.log(triggerPhrases);
+    });
     for(i = 0; i < louis.length; i++) {
       if (message == louis[i] || message.includes(louis[i])) { //If Louis starts projecting, we'll do something!
         bot.sendMessage({ //We're going to send him a message!
