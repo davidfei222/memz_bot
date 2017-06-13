710d20881e858dec2bf186d826fb5f59644b1e9d
davidfei222 <dfei@wisc.edu>
2017-06-12 20:02:02 -0500

    Increased the delay

--- memz_bot.js
+++ memz_bot.js
@@ -63,7 +63,7 @@ function startup(token)
 						to : "285182845519921152", //"285178566751158273", 
 						message : "lambo" //"https://cdn.discordapp.com/attachments/232467376665264128/278646568037384193/42.jpg"
 					});
-				}, 1000);
+				}, 5000);
 			}
 		}
 
