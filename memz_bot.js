8d0aa6638d7e4903d542be164d5e8df500df8c15
davidfei222 <dfei@wisc.edu>
2017-06-12 20:20:07 -0500

    More delay restructuring

--- memz_bot.js
+++ memz_bot.js
@@ -58,6 +58,7 @@ function startup(token)
 		if (rawMsg == "attack_louis") {
 			console.log("Spamming Louis...");
 			for(var i = 0; i < 10; i++) {
+				setTimeout(function () {console.log("Waiting...");}, 1000);
 				bot.sendMessage({
 					to : "285182845519921152", //"285178566751158273", 
 					message : "lambo" //"https://cdn.discordapp.com/attachments/232467376665264128/278646568037384193/42.jpg"
