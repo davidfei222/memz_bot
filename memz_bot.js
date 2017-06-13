e7875c400bc8de04dd9455b2a09310ef273c0c87
davidfei222 <dfei@wisc.edu>
2017-06-12 20:29:32 -0500

    Longer delay

--- memz_bot.js
+++ memz_bot.js
@@ -58,13 +58,20 @@ function startup(token)
 		if (rawMsg == "attack_louis") {
 			console.log("Spamming Louis...");
 			for(var i = 0; i < 10; i++) {
-				setTimeout(function () {console.log("Waiting...");}, 1000);
-				bot.sendMessage({
+				setTimeout(function () {
+					bot.sendMessage({
+						to : "285182845519921152", //"285178566751158273", 
+						message : "lambo" //"https://cdn.discordapp.com/attachments/232467376665264128/278646568037384193/42.jpg"
+					}, function(error, response) {
+						setTimeout(function () {console.log("Waiting...");}, 4000);
+					});
+				}, 4000);
+				/*bot.sendMessage({
 					to : "285182845519921152", //"285178566751158273", 
 					message : "lambo" //"https://cdn.discordapp.com/attachments/232467376665264128/278646568037384193/42.jpg"
 				}, function(error, response) {
 					setTimeout(function () {console.log("Waiting...");}, 1000);
-				});
+				});*/
 			}
 		}
 
